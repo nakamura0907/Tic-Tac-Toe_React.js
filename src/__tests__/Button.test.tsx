3d97d2cfb7877dc536c0fc00a6da6ca84003cbee
ritsuki0907 <ritsuki990907@gmail.com>
2021-03-15 00:04:39 +0900

    ボタンコンポーネントテスト作成 #9

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-import Button, { Props } from '../components/atoms/Button/index';
+import Button, { Props } from 'components/atoms/Button';
 
 const renderButton = (props: Partial<Props> = {}): any => {
   const defaultProps: Props = {
     className: 'button',
     text: 'Click',
     handleClick() {
+      jest.fn();
       return;
     },
   };
@@ -15,18 +16,50 @@ const renderButton = (props: Partial<Props> = {}): any => {
 };
 
 describe('Button', () => {
+  let container;
+  let element;
+
+  test('check default props', () => {
+    container = renderButton().container;
+    element = container.querySelector('button');
+    expect(element.classList.contains('button')).toBe(true);
+    expect(element.textContent).toEqual('Click');
+
+    fireEvent.click(element);
+    // screen.debug();
+  });
+
   test('check className props', () => {
-    renderButton();
-    // screen.debug();
+    container = renderButton({
+      className: 'firstClass secondClass',
+    }).container;
+    element = container.querySelector('button');
+    expect(element.classList.contains('button')).toBe(false);
+    expect(element.classList.contains('firstClass')).toBe(true);
+    expect(element.classList.contains('secondClass')).toBe(true);
 
-    renderButton({
-      className: 'test',
-    });
     // screen.debug();
+  });
 
-    renderButton({
-      className: 'test test2',
-    });
+  test('check text props', () => {
+    container = renderButton({
+      text: 'Button Element',
+    }).container;
+    element = container.querySelector('button');
+    expect(element.textContent).toEqual('Button Element');
+
+    // screen.debug();
+  });
+
+  test('check handleClick props', () => {
+    const handleClick = jest.fn();
+    container = renderButton({
+      handleClick: handleClick,
+    }).container;
+    element = container.querySelector('button');
+    fireEvent.click(element);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+
     // screen.debug();
   });
 });
---
+++ src/__tests__/Copyright.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import Copyright, { Props } from 'components/atoms/Copyright';
+
+describe('Copyright', () => {
+  it('check default props', () => {});
+});
